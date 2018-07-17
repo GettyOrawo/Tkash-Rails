2de5fc2cbf8d1d683d645cceb2c03b8460c0df9f
GettyOrawo <gettyorawo@gmail.com>
2018-07-17 15:05:00 +0300

    Add validations for Pin and Accountnumber

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,9 @@
 class Account < ApplicationRecord
+  validates :balance, numericality: {only_integer: true}
+  validates :balance, numericality: {greater_than: 0}
+  validates :balance, numericality: {less_than: 3000}
+  validates :pin, length: { minimum: 4}
+  validates :pin, numericality: {only_integer: true}
+  validates :accountnumber, numericality: {only_integer: true}
+  validates :accountnumber, uniqueness: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,13 +1,29 @@
 require 'rails_helper'
-
 RSpec.describe Account, type: :model do
   describe "db structure" do
-  it {is_expected.to have_db_column(:balance)}
-  it {is_expected.to have_db_column(:pin)}
-  it {is_expected.to have_db_column(:accountnumber)}
-  it {is_expected.to have_db_column(:id)}
+    it {is_expected.to have_db_column(:balance)}
+    it {is_expected.to have_db_column(:pin)}
+    it {is_expected.to have_db_column(:accountnumber)}
+    it {is_expected.to have_db_column(:id)}
   end
   it "has valid factories" do
-expect(FactoryBot.build(:account)).to be_valid
+    expect(FactoryBot.build(:account)).to be_valid
+  end
+  describe "validations" do
+    context "account balance" do
+    it {is_expected.to validate_numericality_of(:balance).only_integer}
+    it {is_expected.to validate_numericality_of(:balance).is_less_than(3000)}
+    it {is_expected.to validate_numericality_of(:balance).is_greater_than(0)}
+    end
+
+    context "account pin" do
+    it {is_expected.to validate_numericality_of(:pin).only_integer}
+    # it {is_expected.to validate_length_of(:pin).is_at_least(4)}
+    end
+    
+    context "account number" do
+    it {is_expected.to validate_numericality_of(:accountnumber).only_integer}
+    it {is_expected.to validate_uniqueness_of(:accountnumber)}
+    end
   end
 end
