cde2797889215bdcda675bf45e373667e8e9a422
GettyOrawo <gettyorawo@gmail.com>
2018-07-17 13:49:41 +0300

    Add factories

---
+++ spec/factories/accounts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :account do
+    balance 1000
+    pin 1234
+    accountnumber 254711903303
+  end
+end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe Account, type: :model do
   it {is_expected.to have_db_column(:accountnumber)}
   it {is_expected.to have_db_column(:id)}
   end
+  it "has valid factories" do
+expect(FactoryBot.build(:account)).to be_valid
+  end
 end
