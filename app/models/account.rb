aa7b82fd034a5c487c478f3d2f06fda640999df2
GettyOrawo <gettyorawo@gmail.com>
2018-07-18 09:56:34 +0300

    Refactor validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,6 @@
 class Account < ApplicationRecord
   validates :balance, numericality: {only_integer: true}
-  validates :balance, numericality: {greater_than: 0}
-  validates :balance, numericality: {less_than: 3000}
+  validates :balance, numericality: {greater_than: 0, less_than: 3000}
   validates :pin, length: { minimum: 4}
   validates :pin, numericality: {only_integer: true}
   validates :accountnumber, numericality: {only_integer: true}
